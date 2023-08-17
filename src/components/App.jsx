1aba8253d469ea7f8506e497df40f6e0786e9629
njcushing <129661299+njcushing@users.noreply.github.com>
2023-08-17 10:44:21 +0100

    Remove React+Vite boilerplate and create empty MemoryCard component

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './../styles/App.css'
+
+import MemoryCard from './MemoryCard.jsx';
+
+function App() { 
+    return (
+        <MemoryCard />
+    )
+}
+
+export default App;
---
+++ src/components/MemoryCard.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './../styles/MemoryCard.css'
+
+const MemoryCard = () => {
+
+    return (
+        <>
+        </>
+    )
+
+}
+
+export default MemoryCard;
