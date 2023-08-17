341b431d35dd417e5ea7f1c4983ddcf42aac93a2
njcushing <129661299+njcushing@users.noreply.github.com>
2023-08-17 11:34:46 +0100

    Create function that generates array of Card components

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -1,14 +1,32 @@
 import { useState } from 'react'
 import './../styles/MemoryCard.css'
 
+import Card from './Card.jsx'
+
+const createCards = (quantity) => {
+    const cards = [];
+    for (let i = 0; i < quantity; i++) {
+        cards.push(<Card numberValue={i} key={i} />)
+    }
+    return cards;
+}
+
 const MemoryCard = () => {
+    const [cardsQuantity, setCardsQuantity] = useState(10);
 
     const titleComponent = (
         <h1 className={"memory-card-title"}>Memory Card Game</h1>
     )
 
+    const cardsComponent = (
+        <div className={"memory-cards-container"}>
+            {createCards(cardsQuantity)}
+        </div>
+    )
+
     return (<div className="MemoryCard">
         {titleComponent}
+        {cardsComponent}
     </div>)
 
 }
