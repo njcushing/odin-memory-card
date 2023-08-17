ede70ef902f73bd9ff31765af1e0741eae2e16d5
njcushing <129661299+njcushing@users.noreply.github.com>
2023-08-17 11:16:11 +0100

    Create new Card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './../styles/Card.css'
+
+const Card = ({
+
+    numberValue = 0,
+
+}) => {
+
+    return (<div className="card">
+    </div>)
+
+}
+
+export default Card;
